a69e9a4df5fff41f3e7f336b703bf87f2a908646
Pavankumar Batchu <v.n.s.pavankumar.batchu@gmail.com>
2025-10-26 18:03:33 +0530

    Added table booking API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,6 +4,8 @@ from .import views
 
 urlpatterns=[
     path('',views.index,name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     #path('',views.home,name="home"),
     #path('about/',views.about,name="about"),
     #path('book/',views.book,name="book"),
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework import generics,viewsets
+from .models import Menu,Booking
+from .serializers import MenuSerializer,BookingSerializer
 # Create your views here.
 
 
@@ -12,3 +14,14 @@ def index(request):
     main_data={"menu":menu_data}
     return render(request,'menu.html',{"menu":main_data})
     '''
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset=Booking.objects.all()
+    serializer_class=BookingSerializer
